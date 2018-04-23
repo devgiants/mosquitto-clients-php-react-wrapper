4fc3e5fa6b976f25686d3b548022312d687e8a79
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-04-23 18:07:16 +0200

    Basic sub and pub working

---
+++ README.md
@@ -0,0 +1,21 @@
+# React PHP Mosquitto clients wrapper
+
+## Preconfiguration
+Use [config files setup](https://mosquitto.org/man/mosquitto_pub-1.html), such as :
+
+```
+# Host
+-h 192.168.1.2
+-p 1883
+
+# Protocol used
+-V mqttv311
+
+# Last known good (retain)
+-r
+
+# Username and password
+-u test
+-P password
+
+```
--- src/MosquittoClientsReactWrapper/Client/MosquittoClientsReactWrapper.php
+++ src/MosquittoClientsReactWrapper/Client/MosquittoClientsReactWrapper.php
@@ -8,7 +8,10 @@
 
 namespace devgiants\MosquittoClientsReactWrapper\Client;
 
+use devgiants\MosquittoClientsReactWrapper\Exception\MosquittoClientsMissingConfigFileException;
 use devgiants\MosquittoClientsReactWrapper\Exception\MosquittoClientsMissingException;
+use Evenement\EventEmitterTrait;
+use React\EventLoop\LoopInterface;
 
 
 /**
@@ -18,27 +21,69 @@ use devgiants\MosquittoClientsReactWrapper\Exception\MosquittoClientsMissingExce
 class MosquittoClientsReactWrapper {
 
 	/**
-	 * @param string $configFilename
-	 *
+	 * @var LoopInterface
+	 */
+	protected $loop;
+
+	use EventEmitterTrait;
+
+	/**
 	 * @return static
+	 * @throws MosquittoClientsMissingConfigFileException
 	 * @throws MosquittoClientsMissingException
 	 */
-	public static function create(string $configFilename) {
+	public static function create(LoopInterface $loop) {
+
+		// Check clients are installed
 		if(!static::hasDependency()) {
 			throw new MosquittoClientsMissingException("Mosquitto clients are not installed. Try apt-get install mosquitto-clients");
 		}
 
-		return new static([]);
+		// Check config files presents
+		if(!file_exists($_SERVER['HOME']. '/.config/mosquitto_pub')) {
+			throw new MosquittoClientsMissingConfigFileException("~/.config/mosquitto_pub file is missing");
+		}
+
+		if(!file_exists($_SERVER['HOME'] . '/.config/mosquitto_sub')) {
+			throw new MosquittoClientsMissingConfigFileException("~/.config/mosquitto_sub file is missing");
+		}
+
+
+		return new static($loop);
 	}
 
 
 	/**
 	 * MosquittoClientsReactWrapper constructor.
 	 *
-	 * @param array $data the configuration data
+	 * @param LoopInterface $loop
 	 */
-	private function __construct(array $data) {
+	private function __construct(LoopInterface $loop) {
+		$this->loop = $loop;
+	}
 
+
+	/**
+	 * @param string $topic
+	 * @param string $message
+	 */
+	public function publish(string $topic, string $message) {
+		popen("mosquitto_pub -t $topic -m \"$message\"", 'r');
+	}
+
+
+	/**
+	 * @param string $topic
+	 * @param $callback
+	 */
+	public function subscribe(string $topic, $callback) {
+		$this->on( $topic, $callback );
+		echo $topic . PHP_EOL;
+
+		$this->loop->addReadStream( popen( "mosquitto_sub -t \"$topic\"", 'r' ), function ($stream) use ($topic, $callback) {
+			$message = fread($stream, 4096);
+			$this->emit($topic, ['message' => $message]);
+		} );
 	}
 
 	/**
