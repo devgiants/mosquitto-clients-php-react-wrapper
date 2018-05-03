2763e8ec93aac9d27a591e5df55c35d81c088728
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-05-03 12:08:09 +0200

    Directory reoganization + Codes addition

---
+++ src/Code/Codes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 02/05/18
+ * Time: 10:46
+ */
+
+namespace Devgiants\MosquittoClientsReactWrapper\Code;
+
+
+final class Codes {
+	// Mosquitto
+	const MOSQUITTO_CLIENTS_NOT_INSTALLED = 10;
+	const MOSQUITTO_SUB_CONFIG_MISSING = 11;
+	const MOSQUITTO_PUB_CONFIG_MISSING = 12;
+
+	// Internet/Network
+	const INTERNET_NOT_AVAILABLE = 20;
+}
